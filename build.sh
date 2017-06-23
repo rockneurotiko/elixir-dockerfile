7ab00bcfafecc5c8b9acfa08786d79b26b638c19
Rock Neurotiko <miguelglafuente@gmail.com>
2017-06-23 18:57:11 +0200

    Update to elixir 1.4.5

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t rockneurotiko/elixir:1.4.0 .
+docker build -t rockneurotiko/elixir:1.4.5 .
 
-# docker push rockneurotiko/elixir:1.4.0
+docker push rockneurotiko/elixir:1.4.5
